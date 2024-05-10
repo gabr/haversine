fac9bcff671dad10b02e30d978a6b1096c3b5609
gabr <arkadiuszgabrys@gmail.com>
2024-05-10 08:46:36 +0200

    generate-data - printing out info with datetime
    A quick and lazy DateTime printing implementation.
    
    Also changed the brackets around types in help message
    to not confuse user with optional [seed] parameter.
    
    And moved const declarations below help message.

--- generate-data.zig
+++ generate-data.zig
@@ -2,15 +2,6 @@
 // -fno-llvm           disables LLVM to have faster compile time
 // -fno-lld            disables LLD to have faster linking time
 // -fno-error-tracing  print only simple error message on error
-const std    = @import("std");
-const stderr = std.debug.print;
-const fs     = std.fs;
-const assert = std.debug.assert;
-const mem    = std.mem;
-const math   = std.math;
-
-// This is intentionally here and not inside Args struct because
-// it acts also as a documentation for the source code reader.
 const help =
 \\generate-data.zig
 \\  A program for generating random latitude and longitude point
@@ -19,10 +10,10 @@ const help =
 \\Usage:
 \\  ./generate-data.zig output name count [seed]
 \\
-\\    output  [string]  Existing location in which output files will be generated.
-\\    name    [string]  Name prefix used for each of the generated output files.
-\\    count   [u64]     Positive numeric value of how many pairs to generate.
-\\    seed    [u64]     Optional positive numeric value used as a seed for randomness.
+\\    output  <string>  Existing location in which output files will be generated.
+\\    name    <string>  Name prefix used for each of the generated output files.
+\\    count   <u64>     Positive numeric value of how many pairs to generate.
+\\    seed    <u64>     Optional positive numeric value used as a seed for randomness.
 \\
 \\The output of the program are files:
 \\
@@ -45,6 +36,13 @@ const help =
 \\
 ;
 
+const std    = @import("std");
+const stderr = std.debug.print;
+const fs     = std.fs;
+const assert = std.debug.assert;
+const mem    = std.mem;
+const math   = std.math;
+
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     const allocator = arena.allocator();
@@ -53,27 +51,9 @@ pub fn main() !void {
     // Same will go for all the allocations that will follow.
     const args = try Args.get(allocator);
     var out  = try Output.init(allocator, args);
-
-    try out.writer[@intFromEnum(File.info_txt)].print("Test\n", .{});
-
-    for (0..args.count) |i| { stderr("i: {}\n", .{i}); }
-
-    // latitude  (y): [ -90;  90]
-    // longitude (x): [-180; 180]
-    // {"pairs":[
-    //     {"x0":102.1633205722960440, "y0":-24.9977499718717624, "x1":-14.3322557404258362, "y1":62.6708294856625940},
-    //     {"x0":106.8565762320475301, "y0":11.9375999763435772,  "x1":160.8862834978823457, "y1":69.1998577207801731},
-    //     {"x0":-8.8878390193835415,  "y0":19.0969814947109811,  "x1":-46.8743926814281267, "y1":43.8734538780514995},
-    //     {"x0":-14.6150077801348424, "y0":38.3262373436592725,  "x1":-44.3948041164556955, "y1":53.2633096739310474},
-    //     {"x0":152.2097004499833020, "y0":28.8075361257721383,  "x1":150.6987913536557357, "y1":32.0449908116227959},
-    //     {"x0":163.7513986349316610, "y0":32.3825205307932791,  "x1":152.5987721841468954, "y1":29.8302328719929157},
-    //     {"x0":131.0675707860914372, "y0":47.0663770222409354,  "x1":123.5929119363244411, "y1":46.3284990503599943},
-    //     {"x0":122.1332570844595011, "y0":47.1276485830673622,  "x1":122.2277624036606340, "y1":45.8344893091440824},
-    //     {"x0":177.4032764475290946, "y0":-29.8936958880840180, "x1":93.6601882829341008,  "y1":-79.6531326321997426},
-    //     {"x0":8.1610847750272519,   "y0":-38.2671013246085465, "x1":14.7521018520305667,  "y1":-61.2419556816616790}
-    // ]}
-
-    try out.deinit();
+    // doing it like that to at least try to flush the data out in case of any error
+    defer out.deinit() catch |err| stderr("failed to deinit output files\nerror: {s}\n", .{@errorName(err)});
+    try generate(allocator, args, out);
 }
 
 const Args = struct {
@@ -146,6 +126,12 @@ const Output = struct {
         self.dir.close();
     }
 
+    /// Puts message both into info.txt file and stderr
+    pub fn info(self: *Output, comptime fmt: []const u8, args: anytype) !void {
+        stderr(fmt, args);
+        try self.writer[@intFromEnum(File.info_txt)].print(fmt, args);
+    }
+
     /// Creates file with given name prefix and suffix in given directory.
     /// Will fail if file already exists.
     fn createFile(allocator: mem.Allocator, outdir: fs.Dir, name: []const u8, suf: []const u8) !fs.File {
@@ -157,6 +143,77 @@ const Output = struct {
         };
     }
 };
+
+// typed out on one breath - somehow tested - should work
+fn getDateTime() ![]const u8 {
+    const Static = struct { var buf: ["yyyy-mm-ddThh:mm:ssZ".len]u8 = undefined; };
+    const sec_per_min  : u32 = 60;
+    const sec_per_hour : u32 = 60*sec_per_min;
+    const sec_per_day  : u32 = 24*sec_per_hour;
+    const sec_per_year : u32 = 365*sec_per_day;
+    const stimestamp = std.time.timestamp(); // UTC 1970-01-01 in seconds
+    assert(stimestamp>0); // no time travelers
+    var timestamp: u32 = @intCast(stimestamp); // so we can cast to u32
+    const years = @divTrunc(timestamp, sec_per_year);
+    const year = 1970 + years;
+    assert(years>2); // we assume at least 1972 for ease of calculation
+    const leap_years = @divTrunc((years-2), 4)+1;
+    timestamp -= (years*sec_per_year)+((leap_years-2)*sec_per_day);
+    var feb : u8 = 28; if (@rem(year,4)==0) feb += 1;
+    const day : u32 = @divTrunc(timestamp, sec_per_day);
+    timestamp = @rem(timestamp, sec_per_day);
+    var month: u32 = 1;
+    var month_day: u32 = day;
+    const months = [_]u8 { 31, feb, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    var daym: u32 = 0;
+    for (months, 1..) |m, i| {
+        daym += m;
+        if (day < daym) {
+            month = @intCast(i);
+            month_day = day - (daym - m);
+            break;
+        }
+    }
+    const hour = @divTrunc(timestamp, sec_per_hour);
+    timestamp = @rem(timestamp, sec_per_hour);
+    const min = @divTrunc(timestamp, sec_per_min);
+    timestamp = @rem(timestamp, sec_per_min);
+    const sec = timestamp;
+    assert(year>1972 and year<9999);
+    assert(month>0 and month<13);
+    assert(month_day>0 and month_day<32);
+    assert(hour>=0 and hour<25);
+    assert(min>=0 and min<61);
+    assert(sec>=0 and sec<61);
+    return try std.fmt.bufPrint(&Static.buf,
+        "{d:0>4}-{d:0>2}-{d:0>2}T{d:0>2}:{d:0>2}:{d:0>2}Z",
+        .{year, month, month_day, hour, min, sec});
+}
+
+fn generate(allocator: mem.Allocator, args: Args, out: *Output) !void {
+    // write out startup info data
+    try out.info("time:  {s}\n", .{try getDateTime()});
+    try out.info("cmd:   {s}\n", .{try mem.join(allocator, " ", args.args)});
+    try out.info("out:   {s}\n", .{args.out});
+    try out.info("name:  {s}\n", .{args.name});
+    try out.info("count: {d}\n", .{args.count});
+    try out.info("seed:  {d}\n", .{args.seed});
+
+    // latitude  (y): [ -90;  90]
+    // longitude (x): [-180; 180]
+    // {"pairs":[
+    //     {"x0":102.1633205722960440, "y0":-24.9977499718717624, "x1":-14.3322557404258362, "y1":62.6708294856625940},
+    //     {"x0":106.8565762320475301, "y0":11.9375999763435772,  "x1":160.8862834978823457, "y1":69.1998577207801731},
+    //     {"x0":-8.8878390193835415,  "y0":19.0969814947109811,  "x1":-46.8743926814281267, "y1":43.8734538780514995},
+    //     {"x0":-14.6150077801348424, "y0":38.3262373436592725,  "x1":-44.3948041164556955, "y1":53.2633096739310474},
+    //     {"x0":152.2097004499833020, "y0":28.8075361257721383,  "x1":150.6987913536557357, "y1":32.0449908116227959},
+    //     {"x0":163.7513986349316610, "y0":32.3825205307932791,  "x1":152.5987721841468954, "y1":29.8302328719929157},
+    //     {"x0":131.0675707860914372, "y0":47.0663770222409354,  "x1":123.5929119363244411, "y1":46.3284990503599943},
+    //     {"x0":122.1332570844595011, "y0":47.1276485830673622,  "x1":122.2277624036606340, "y1":45.8344893091440824},
+    //     {"x0":177.4032764475290946, "y0":-29.8936958880840180, "x1":93.6601882829341008,  "y1":-79.6531326321997426},
+    //     {"x0":8.1610847750272519,   "y0":-38.2671013246085465, "x1":14.7521018520305667,  "y1":-61.2419556816616790}
+    // ]}
+}
 
 inline fn square(x: f64) f64 { return math.pow(f64, x, 2); }
 
