0a9ab3611ca1493ad964d6da98c65710f40dfa54
gabr <arkadiuszgabrys@gmail.com>
2024-05-14 21:57:59 +0200

    extracting floats from JSON

--- local.vim
+++ local.vim
@@ -1,3 +1,3 @@
 noremap <silent> <F9> :!rm data/*; time ./generate-data.zig data three 3 \| pv -t; ls -alh data/; cat data/*json; echo "data.f64:"; xxd -g 1 data/*-data.f64; echo "hsin.f64:"; xxd -g 1 data/*-hsin.f64; echo end<CR>
 
-noremap <silent> <F10> :!time ./parse-data.zig \| pv -t<CR>
+noremap <silent> <F10> :!time ./parse-data.zig data three \| pv -t<CR>
--- parse-data.zig
+++ parse-data.zig
@@ -1,4 +1,4 @@
-///usr/bin/env zig run -fno-llvm -fno-lld -fno-error-tracing "$0" -- "$@"; exit
+///usr/bin/env zig run -fno-llvm -fno-lld -fno-error-tracing -freference-trace "$0" -- "$@"; exit
 // -fno-llvm           disables LLVM to have faster compile time
 // -fno-lld            disables LLD to have faster linking time
 // -fno-error-tracing  print only simple error message on error
@@ -52,7 +52,8 @@ const help =
 ;
 
 const std       = @import("std");
-//const dstderr   = std.debug.print;
+const io        = std.io;
+const dstderr   = std.debug.print;
 const fs        = std.fs;
 const assert    = std.debug.assert;
 const mem       = std.mem;
@@ -64,9 +65,12 @@ pub fn main() !void {
     // Lack of arena.deinit() is intentional as there is no
     // need to free the memory in this short living program.
     // Same will go for all the allocations that will follow.
-    const args = try Args.get(allocator); _ = args;
-    const stdout = std.io.getStdOut().writer();
-    try stdout.writeAll("output\n");
+    const args = try Args.get(allocator);
+    const result =
+        if (args.valid) try parseAndValidate(allocator, args)
+                   else try parse(allocator, args);
+    const stdout = io.getStdOut().writer();
+    try stdout.print("{d}\n", .{result});
 }
 
 const Args = struct {
@@ -77,7 +81,7 @@ const Args = struct {
 
     pub fn get(allocator: mem.Allocator) !Args {
         var a: Args = undefined;
-        const stderr = std.io.getStdErr().writer();
+        const stderr = io.getStdErr().writer();
         a.args = try std.process.argsAlloc(allocator);
         if (a.args.len < 3) { try stderr.writeAll(help); return error.NotEnoughArguments; }
         a.path = a.args[1];
@@ -86,4 +90,81 @@ const Args = struct {
         return a;
     }
 };
+
+/// Supply with the a Reader type
+fn JSON_Reader(comptime T: type) type {
+    return struct {
+        creader: io.CountingReader(T),
+        reader:  io.CountingReader(T).Reader,
+
+        const Self = @This();
+
+        /// Pass the reader instance to read JSON from
+        pub fn init(reader: T) Self {
+            var cr = io.countingReader(reader);
+            return .{
+                .creader = cr,
+                .reader  = cr.reader(),
+            };
+        }
+
+        /// Moves to the next {
+        pub fn nextObject(self: *Self) !void {
+            while (try self.reader.readByte() != '{') {}
+        }
+
+        pub fn nextKey(self: *Self) ![]const u8 {
+            const Static = struct { var buf: [255]u8 = undefined; };
+            while (try self.reader.readByte() != '"') {}
+            return try self.reader.readUntilDelimiter(&Static.buf, '"');
+        }
+
+        pub fn nextFloat(self: *Self) !f64 {
+            const Static = struct { var buf: [255]u8 = undefined; };
+            var b = try self.reader.readByte();
+            if (b != ':') {
+                dstderr("expected ':' but found '{c}' at {d} byte\n", .{ b, self.creader.bytes_read });
+                return error.JSONUnexpectedCharacter;
+            }
+            b = try self.reader.readByte();
+            while (std.ascii.isWhitespace(b)) { b = try self.reader.readByte(); }
+            var i: usize = 0;
+            while (b == '-' or b == '.' or std.ascii.isDigit(b)) {
+                Static.buf[i] = b; i+= 1;
+                b = try self.reader.readByte();
+            }
+            const float_str = mem.trim(u8, Static.buf[0..i], " ");
+            return try std.fmt.parseFloat(f64, float_str);
+        }
+    };
+}
+
+fn parse(allocator: mem.Allocator, args: Args) !f64 {
+    const file_name = try mem.join(allocator, "-", &[_][]const u8{ args.name, "data.json" });
+    const file_path = try fs.path.join(allocator, &[_][]const u8{ args.path, file_name });
+    const file = try fs.cwd().openFile(file_path, .{});
+    const stat = try file.stat();
+    const bufreader = io.bufferedReader(file.reader());
+    var jsonr = JSON_Reader(@TypeOf(bufreader)).init(bufreader);
+    try jsonr.nextObject();
+    const pairs_key = try jsonr.nextKey();
+    if (!mem.eql(u8, pairs_key, "pairs")) {
+        dstderr("pairs key not found\n", .{});
+        return error.JSONIncorrectFormat;
+    }
+    for (0..stat.size) |_| {
+        jsonr.nextObject() catch |err| switch (err) { error.EndOfStream => break, else => return err, };
+        _ = try jsonr.nextKey(); const x0 = try jsonr.nextFloat(); dstderr("{d}, ", .{x0});
+        _ = try jsonr.nextKey(); const y0 = try jsonr.nextFloat(); dstderr("{d}, ", .{y0});
+        _ = try jsonr.nextKey(); const x1 = try jsonr.nextFloat(); dstderr("{d}, ", .{x1});
+        _ = try jsonr.nextKey(); const y1 = try jsonr.nextFloat(); dstderr("{d}\n", .{y1});
+    }
+    return 0;
+}
+
+fn parseAndValidate(allocator: mem.Allocator, args: Args) !f64 {
+    _ = allocator;
+    _ = args;
+    return error.NotImplemented;
+}
 
