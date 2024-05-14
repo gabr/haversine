909ebfafa1d54ab845bd6f0238579a9b64fbad08
gabr <arkadiuszgabrys@gmail.com>
2024-05-14 19:37:09 +0200

    improved scripts
    Added comments and release.sh can now build parse-data.zig
    It builds only if the source code files are newer then the binary.
    
    Also and added parse-data to .gitignore

--- release.sh
+++ release.sh
@@ -1,8 +1,17 @@
 #/bin/bash
+# A script to build release versions of each program for speed.
 set -eu # exit on error and if there is unassigned variable use
-file="generate-data"
-[ -f $file   ] && rm $file
-[ -f $file.o ] && rm $file.o
-set -x # show the executed command
-zig build-exe -O ReleaseFast $file.zig
-rm $file.o # don't need that
+build() {
+    local -r file="$1"; shift
+    [ $file.zig -ot $file ] && echo $file.zig no changes && return 0
+    [ -f $file   ] && rm $file
+    [ -f $file.o ] && rm $file.o
+    ( # use subshell to disable set -x after rm command
+        set -x # show the executed command
+        zig build-exe -O ReleaseFast $file.zig
+        rm $file.o # don't need that
+    )
+}
+
+build "generate-data"
+build "parse-data"
--- validate-json.sh
+++ validate-json.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+# A helper script to validate if generated JSON files are correct.
 set -eu # exit on error and if there is unassigned variable use
 # provide file path as first argument to the script
 json_pp < "$1" 1>/dev/null
