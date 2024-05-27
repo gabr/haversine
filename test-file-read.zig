03373e1937791ad23b400c08e280e1689dda9020
gabr <arkadiuszgabrys@gmail.com>
2024-05-27 13:40:10 +0200

    improve testtest-file-read.zig

--- test-file-read.zig
+++ test-file-read.zig
@@ -9,31 +9,42 @@ const assert    = std.debug.assert;
 const prof      = @import("prof.zig");
 
 const profEnabled = true;
-const ProfArea = enum { big_json1, big_json2, small_json1, small_json2 };
+const ProfArea = enum {
+    big_json1,    big_json2,
+    small_json1,  small_json2,
+    big_float1,   big_float2,
+    small_float1, small_float2,
+};
 var gprof: prof.Profiler(profEnabled, ProfArea) = .{};
 
 pub fn main() !void {
     gprof.init();
-    try testFile("data/big-data.json",     .big_json1);
-    try testFile("data/big-data.json",     .big_json2);
+    try testFile("data/big-data.json",   .big_json1);
+    try testFile("data/big-data.json",   .big_json2);
     try testFile("data/three-data.json", .small_json1);
     try testFile("data/three-data.json", .small_json2);
+    try testFile("data/big-data.f64",    .big_float1);
+    try testFile("data/big-data.f64",    .big_float2);
+    try testFile("data/three-data.f64",  .small_float1);
+    try testFile("data/three-data.f64",  .small_float2);
     try gprof.sum(io.getStdErr().writer());
 }
 
 pub fn testFile(path: []const u8, comptime area: ProfArea) !void {
-    const file = try fs.cwd().openFile(path, .{}); defer file.close();
-    const file_size = (try file.stat()).size;
-    gprof.area_data[@intFromEnum(area)] += file_size;
-    const bufsiz = 4096;
-    const BufType = prof.ProfiledBufferedReader(profEnabled, bufsiz, fs.File.Reader, ProfArea, area);
-    var bufdjr: BufType = .{ .inner_reader = file.reader(), .profiler = &gprof, };
-    const reader = bufdjr.reader();
-    var buf: [bufsiz]u8 = undefined;
-    var read: usize = 0;
-    gprof.start(area);
-    while (read < file_size) {
-        read += try reader.read(&buf);
+    for (1..1000) |_| {
+        const file = try fs.cwd().openFile(path, .{}); defer file.close();
+        const file_size = (try file.stat()).size;
+        gprof.area_data[@intFromEnum(area)] += file_size;
+        const bufsiz = 4096;
+        const BufType = prof.ProfiledBufferedReader(profEnabled, bufsiz, fs.File.Reader, ProfArea, area);
+        var bufdjr: BufType = .{ .inner_reader = file.reader(), .profiler = &gprof, };
+        const reader = bufdjr.reader();
+        var buf: [bufsiz]u8 = undefined;
+        var read: usize = 0;
+        gprof.start(area);
+        while (read < file_size) {
+            read += try reader.read(&buf);
+        }
+        gprof.end(area);
     }
-    gprof.end(area);
 }
